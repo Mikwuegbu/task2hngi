8477ff3924ecdb51dafc2f94f2473863d29362c7
Mark Stephen <74438762+Mikwuegbu@users.noreply.github.com>
2023-09-08 17:16:17 +0100

    Updated Script

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let displayDayAndTime = () => {
     const dayOfTheWeek = document.getElementById('currentDay');
     const utcCurrentTime = document.getElementById('currentUTCTime');
 
-    const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const daysOfWeek = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     const now = new Date();
 
     const dayOfWeek = daysOfWeek[now.getUTCDay()];
